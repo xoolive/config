787c12a63d39c0317c67d89b5293e53df52c9cb1
Xavier Olive <xavier@xoolive.org>
2014-04-29 01:22:58 +0200

    Small corrections

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,6 +2,7 @@
 git config --global alias.gl "log -12 --color=always --all --graph --topo-order --pretty='format:%Cgreen%h%Creset %s %C(black bold)(by %an)%Creset%C(yellow bold)%d%Creset%n'"
 git config --global alias.ll "log -18 --color=always --all --topo-order --pretty='format:%Cgreen%h%Creset %s%Cred%d%Creset %C(black bold)(by %an)%Creset'"
 git config --global alias.root "rev-parse --show-toplevel"
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
 
 # color
 git config --global color.branch auto
@@ -16,3 +17,4 @@ git config --global user.name "Xavier Olive"
 
 # how to push (branch)
 git config --global push.default simple
+git config --global pull.ff only
--- vimrc
+++ vimrc
@@ -356,11 +356,12 @@ elseif has("win32")
 
 endif
 
-" let g:airline_powerline_fonts = 1
+let g:airline_powerline_fonts = 1
 let g:airline_left_sep = '⮀'
 let g:airline_left_alt_sep = '⮁'
 let g:airline_right_sep = '⮂'
 let g:airline_right_alt_sep = '⮃'
+let g:airline_symbols = {}
 let g:airline_symbols.branch = '⭠'
 let g:airline_symbols.readonly = '⭤'
 let g:airline_symbols.linenr = '⭡'
