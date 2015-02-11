dd0186c0ac3a1513b1c17d4888eeea914c90ae85
Xavier Olive <xavier@xoolive.org>
2015-02-11 23:26:24 +0100

    Emulate conda activate function

--- zsh.d/60_functions.zsh
+++ zsh.d/60_functions.zsh
@@ -3,4 +3,10 @@ gpg –keyserver subkeys.pgp.net –recv-keys $1
 gpg –armor –export $1 | sudo apt-key add -
 }
 
+function activate {
+# Play with uname -s
+source $HOME/Library/virtualenv/$1/bin/activate
+}
+
+
 
